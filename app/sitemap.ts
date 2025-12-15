923a6c21c9589871359eb75101c75d5a76b7b2c4
Bridger Tower <bridgertower@gmail.com>
2025-12-14 20:28:45 -0700

    wip

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -19,6 +19,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/work-with-me`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: "monthly",
